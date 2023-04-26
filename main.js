73e9a4a37157f708a37beb3027e6ef1689bab3fa
ggeonWoo <pgw5123@naver.com>
2023-04-26 15:42:49 +0900

    13

--- main.js
+++ main.js
@@ -12,3 +12,27 @@ document.addEventListener('scroll',()=>{
     }
 
 })
+
+//Handle scrolling
+const navbarMenu= document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event)=>{
+    const target= event.target;
+    const link=target.dataset.link;
+    if(link==null){
+        return;
+    }
+   scrollIntoView(link);
+});
+
+
+//contact
+
+const homeConTact=document.querySelector('.home__contact');
+homeConTact.addEventListener('click',()=>{
+    scrollIntoView('#contact')
+
+  })
+  
+  function scrollIntoView(selector){
+    const scrollTo=document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:"smooth"})}
