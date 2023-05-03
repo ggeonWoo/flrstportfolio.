285909211b2cb90498a5fc82195f4cfdd1b08d48
ggeonWoo <pgw5123@naver.com>
2023-05-03 18:08:02 +0900

    콜백연습

--- 연습.js
+++ 연습.js
@@ -1,7 +1,16 @@
+// function add(){
+// document.getElementById('title').innerHTML='아이디입력';
+// document.getElementById('alert').style.display='block';
+// }
+// function dda(){
+//     document.getElementById('title').innerHTML='비번입력';
+//     document.getElementById('alert').style.display='block';
+//     }
 
-function add(){
-    document.getElementById('alert').style.display='block';
-}
-function dda() {
-    document.getElementById('alert').style.display='none'
-}
+    document.getElementById('open').addEventListener('click',function(){
+        document.getElementById('alert').style.display='block';
+    })
+
+    document.getElementById('close').addEventListener('click',function(){
+        document.getElementById('alert').style.display='none';
+    })
